5a00562e90e61531fe85e605b7d036e104ee8a9d
Sushant6291 <98340717+Sushant6291@users.noreply.github.com>
2022-04-18 11:51:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Info from './Info';
+import User from './User';
+
+
+function App() {
+  return (
+    
+    <><Info />
+    <User />
+    </>
+  );
+}
+
+export default App;
